dbe379ca734c2f2e95a58ab1074e0e37a1f376e3
Jashanjot Sohanpal <jashanjot.sohanpal@hotmail.com>
2015-01-09 18:19:09 -0500

    Reddit Searcher

---
+++ Controllers/RedditJsonController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace RedditSearch.Controllers
+{
+    public class RedditJsonController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage resp = new HttpResponseMessage();
+
+            try
+            {
+                string lcUrl = "http://www.reddit.com/.json?limit=100";
+
+                HttpWebRequest loHttp = (HttpWebRequest)WebRequest.Create(lcUrl);
+
+                loHttp.Method = "GET";
+
+                HttpWebResponse loWebResponse = (HttpWebResponse)loHttp.GetResponse();
+                Encoding enc = Encoding.UTF8;
+                StreamReader loResponseStream = new StreamReader(loWebResponse.GetResponseStream(), enc);
+                StringContent lcHtml = new StringContent(loResponseStream.ReadToEnd());
+                loWebResponse.Close();
+                loResponseStream.Close();
+
+                lcHtml.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                resp.Content = lcHtml;
+            }
+            catch
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Reddit Seems Down");
+            }
+            
+            return resp;
+
+        }
+
+    }
+}
---
+++ js/home-index.js
@@ -0,0 +1,21 @@
+﻿// home-index.js
+
+var app = angular.module("myapp", []);
+
+app.controller("homeIndexController", function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/RedditJson")
+    .then(function (result) {
+        angular.copy(result.data, $scope.data);
+        $scope.myValue = false;
+    },
+    function () {
+        $scope.error = "Reddit Seems to be Down";
+        $scope.myValue = true;
+    })
+    .then(function () {
+        $scope.isBusy = false;
+    });
+});
